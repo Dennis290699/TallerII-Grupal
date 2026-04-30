9f6187655e4a938646423f412e4139524230305c
Dennis290699 <trujilloadrian29@gmail.com>
2026-04-30 11:40:33 -0500

    fix: utilizar jpackage para generar ejecutable standalone de Windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("java")
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("edu.sc.seis.launch4j") version "3.0.5"
 }
 
 group = "org.example"
@@ -32,13 +29,4 @@ tasks.shadowJar {
     archiveClassifier.set("")
     archiveVersion.set(project.version.toString())
     mergeServiceFiles()
-}
-
-launch4j {
-    mainClassName.set("com.programacion.Principal")
-    outfile.set("TallerII-App.exe")
-    errTitle.set("Error de Aplicacion")
-    dontWrapJar.set(false)
-    downloadUrl.set("https://adoptium.net/")
-    // icon = "${projectDir}/src/main/resources/icon.ico" // TODO: Descomentar y cambiar a futuro cuando tengas un icono genérico .ico
 }
